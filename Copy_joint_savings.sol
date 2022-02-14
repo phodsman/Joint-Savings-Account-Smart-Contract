73eef770efe21f07881681eaa6e0a214c762c547
Preston Hodsman <phodsman@yahoo.com>
2022-02-13 17:29:19 -0800

    added require to check if funds are insufficient

--- Copy_joint_savings.sol
+++ Copy_joint_savings.sol
@@ -47,12 +47,12 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds` is returned.
         */
-        // YOUR CODE HERE! Insufficient funds
+        require(contractBalance >= amount, "Insufficient funds");
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
         */
-        // YOUR CODE HERE!
+        
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
         // YOUR CODE HERE!
